64424d8eedc90800210687f106686a5e2a538dab
GBShadowTxai <gustavo.sombra@grupotxai.com.br>
2023-02-22 13:50:15 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,56 @@
+import csvtojson from "csvtojson";
+import { createReadStream } from "node:fs";
+import { createServer } from "node:http";
+import { Readable, Transform } from "node:stream";
+import { TransformStream, WritableStream } from "node:stream/web";
+import { setTimeout } from "node:timers/promises";
+
+const PORT = 3000;
+
+createServer(async (request, response) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+  };
+
+  if (request.method === "OPTIONS") {
+    response.writeHead(204, headers);
+    response.end();
+    return;
+  }
+
+  let items = 0;
+  request.once("close", () => console.log("Connection was closed.", items));
+  Readable.toWeb(createReadStream("./animeflv.csv"))
+    .pipeThrough(Transform.toWeb(csvtojson()))
+    .pipeThrough(
+      new TransformStream({
+        transform(chunk, controller) {
+          const data = JSON.parse(Buffer.from(chunk));
+          controller.enqueue(
+            JSON.stringify({
+              title: data.title,
+              description: data.description,
+              url_anime: data.url_anime,
+            }).concat("\n")
+          );
+        },
+      })
+    )
+    .pipeTo(
+      new WritableStream({
+        async write(chunk) {
+          await setTimeout(1000);
+          items++;
+          response.write(chunk);
+        },
+        close() {
+          response.end();
+        },
+      })
+    );
+
+  response.writeHead(200, headers);
+})
+  .listen(PORT)
+  .on("listening", (_) => console.log(`Server is running at ${PORT}`));
